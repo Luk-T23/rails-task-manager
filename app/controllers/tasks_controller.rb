18f45bf0c049237e8ac86b97fcf1a1e14ff825ff
Luk Triquet <luktriquet@gmail.com>
2024-06-04 16:19:08 +0200

    added routes for task controller CRUD

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,17 @@
+class TasksController < ApplicationController
+  def list
+    # shows all tasks
+  end
+
+  def create
+    # creates a new task
+  end
+
+  def update
+    # updates/changes a task
+  end
+
+  def destroy
+    # deletes a task
+  end
+end
